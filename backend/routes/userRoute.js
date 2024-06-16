ec31d334b3b947117d964fc7efd8dad0b6cc61ec
dishant <itsdishantnishad@gmail.com>
2024-06-16 15:52:01 +0530

    adding upcoming group api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const router=express.Router();
-const {registerUser, loginUser, logout, getUser}=require("../controllers/User/userController");
+const {registerUser, loginUser, logout, getUser,upcomingGroups}=require("../controllers/User/userController");
 const { protect } = require("../middleware/authMiddleware");
+
 
 router.post("/register",registerUser);
 router.post("/login",loginUser)
 router.get("/logout",logout)
 router.get("/getUser",protect,getUser)
-
+router.get("/upcomingGroups",upcomingGroups);
 module.exports=router;
