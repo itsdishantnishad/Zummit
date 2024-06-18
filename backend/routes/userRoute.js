efc27e8374a57e21aed5f3e280d11df95ee0111d
dishant <itsdishantnishad@gmail.com>
2024-06-18 10:37:11 +0530

    adding appointmentList API

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const router=express.Router();
 const {registerUser, loginUser, logout, getUser,upcomingGroups}=require("../controllers/User/userController");
+const {appointmentList} = require("../controllers/User/appointmentList");
 const { protect } = require("../middleware/authMiddleware");
 
 
@@ -9,4 +10,5 @@ router.post("/login",loginUser)
 router.get("/logout",logout)
 router.get("/getUser",protect,getUser)
 router.get("/upcomingGroups",upcomingGroups);
+router.get("/appointmentList",appointmentList);
 module.exports=router;
